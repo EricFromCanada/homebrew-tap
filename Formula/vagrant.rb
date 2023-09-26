b76576bc17e860f3468670d637c35ae859bab257
Eric Knibbe <enk3@outlook.com>
2023-09-26 12:23:14 -0400

    vagrant: fix for errors on macOS

--- Formula/vagrant.rb
+++ Formula/vagrant.rb
@@ -4,12 +4,12 @@
 class Vagrant < Formula
   desc "Development environment"
   homepage "https://www.vagrantup.com/"
+  url "https://releases.hashicorp.com/vagrant/2.3.7/vagrant_2.3.7_linux_amd64.zip"
   version "2.3.7"
+  sha256 "4f5f6c55f9cb3ede37cd4a928f023e7ec0a3328125e76ae1166dc7908f1f48cb"
 
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vagrant/2.3.7/vagrant_2.3.7_linux_amd64.zip"
-    sha256 "4f5f6c55f9cb3ede37cd4a928f023e7ec0a3328125e76ae1166dc7908f1f48cb"
-  end
+  depends_on arch: :x86_64
+  depends_on :linux
 
   conflicts_with "vagrant"
 
